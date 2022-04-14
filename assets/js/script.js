838c29ffb07815f6d665a7a277ec0c1d5a4f600e
MSch33 <99237120+mdschenck@users.noreply.github.com>
2022-04-14 00:56:13 -0400

    Merged API return data with HTML elements to display results from API call. Working on Tiles for 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,7 +20,8 @@ var apiKey = "295f6bd4f18e43d6b3b9627ef087838d";
 
 function renderWeather() {
   console.log("RENDERWEATHER CALLED");
-  var url = `${baseURL}geo/1.0/direct?q={citySearch}&limit=1&appid=${apiKey}`;
+  var url = `${baseURL}geo/1.0/direct?q=${locationSearch.value}&limit=1&appid=${apiKey}`;
+  console.log(url);
 
   fetch(url)
     .then(function (response) {
@@ -30,27 +31,77 @@ function renderWeather() {
       var cityObject = data[0];
       var lat = cityObject.lat;
       var lon = cityObject.lon;
+      var cityName = cityObject.name;
       console.log(data);
       console.log(lat);
       console.log(lon);
+      console.log(cityName);
 
-      var currentWeatherUrl = `${baseURL}data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+      var currentWeatherUrl = `${baseURL}data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+      fetch(currentWeatherUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          //   var weatherObject = data[0];
+          var tempCurrent = data.current.temp;
+          var windSpeed = data.current.wind_speed;
+          var humidityLevel = data.current.humidity;
+          var uv = data.current.uvi;
+          var icon = data.current.weather[0].icon;
+
+          //   city.textContent = cityName;   **CLEANER WAY TO DISPLAY MAIN CARD? HTML ELEMENTS??
+          //   temp.textContent = tempCurrent;
+          //   wind.textContent = windSeed;
+          //   humidity.textContent = humidityLevel;
+          //   uvIndex.textContent = uv;
+
+          document.getElementById(
+            "summaryCity"
+          ).innerHTML = `<h2>${cityName}</h2>`;
+          document.getElementById("summaryData").innerHTML = `
+          <dl>
+          <dt>temp:</dt>
+       <dd>${tempCurrent}</dd>
+       <dt>Wind:</dt>
+       <dd>${windSpeed}</dd>
+       <dt>Humidity</dt>
+       <dd>${humidityLevel}</dd>
+       <dt>UV Index</dt>
+       <dd class="uvIdx">${uv}</dd>
+     </dl>
+
+       <h2 class="summaryIcon">${icon} ICON <h2>`;
+        });
+      populate5day();
     });
-  document.getElementById(
-    "summaryCity"
-  ).innerHTML = `<h2>${locationSearch.value}</h2>`;
-  document.getElementById("summaryData").innerHTML = `
-               <dl>
-                  <dt>temp:</dt>
-               <dd>88.88</dd>
-               <dt>Wind:</dt>
-               <dd>88.88 MPH</dd>
-               <dt>Humidity</dt>
-               <dd>88% </dd>
-               <dt>UV Index</dt>
-               <dd class="uvIdx">88</dd>
-             </dl>
-  `;
+
+  function populate5day(data) {
+    console.log("POPULATE % DAY CALLED");
+
+    data.forEach(function (daily) {
+      console.log(`day${i} card run`);
+      var fiveDayTemp = daily.temp;
+      var fiveDayWind = daily.wind;
+      var fiveDayHumidity = daily.humidity;
+      var fiveDayIcon = daily.weather[0].icon;
+
+      document.getElementById(`day${i}`).innerHTML = `
+    <h2>${fiveDayIcon} ICON <h2>
+
+    <dl>
+    <dt>temp:</dt>
+ <dd>${fiveDaytemp}</dd>
+ <dt>Wind:</dt>
+ <dd>${fiveDayWind}</dd>
+ <dt>Humidity</dt>
+ <dd>${fiveDayHumidity}</dd>
+</dl>
+`;
+    });
+  }
 }
 
 //baseUrl + data/2.5/forecast?id=524901&appid= + apiKey;
